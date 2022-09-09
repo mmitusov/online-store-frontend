9204a11c8b245aea9a82f8310a3f2a7d2b3ce045
mmitusov <maksym.mitusov@gmail.com>
2022-09-09 19:46:28 +0300

    AppRouter implementation

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -4,9 +4,13 @@ import { authRoutes } from '../routes';
 
 const AppRouter = () => {
   const isAuth = false //временная заглушка - показывает авторизован пользователь или нет
+  //импортируем массив с роутами доступный авторизованным, пробегаемся по нему с помощью .map() и вытаскиваем из объекта путь и компонент
+  //И для каждого елемента массива мы отрисовываем Route, где указываем путь и компонент который по этому пути должен отрисовывать
   return (
     <Switch>
-      {authRoutes}
+      {authRoutes.map(({path, Component}) => 
+        <Route path={path} component={Component}/>
+      )}
     </Switch>
   )
 }
