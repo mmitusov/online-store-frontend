aaabd67a67b4d32f68822b0cfd1bf396f5a5d692
mmitusov <maksym.mitusov@gmail.com>
2022-09-24 22:35:12 +0300

    NavBar implementation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,60 +3,45 @@
 //После чего експортируем NavBаr и импортируем его в App.js, чтобы он отображался у нас на каждой странице
 //То есть, так как в App.js отображаются все видимые нам компоненты, то сверху мы помещаем NavBar (виден всегда), а под ним AppRouter который будет уже отображать под NavBar тот компонет который будет указан в AppRouter
 //Далее поменяем NavLink из bootstrap на NavLink из react-router-dom, чтобы мы могли переключаться по нашим pages (пользоваться нашим react-router-dom)
-//Далее быстренько настроим стиль нашего нового елемента. P.S. Чтобы не тратить время на создание файлов со стилями, некоторые стили будем прописывать inline прям в HTML елементах 
+//Далее быстренько настроим новый navbar - убераем лишнее, добавляем нужное, меняем стили.
+//Была проблема с ml-auto, но оказалось, что просто в новой версии изменили синтаксис. Новый синтаксис - https://getbootstrap.com/docs/5.1/utilities/spacing/
+//P.S. Чтобы не тратить время на создание файлов со стилями, некоторые стили будем прописывать inline прям в HTML елементах (style={___})
+//Далее воспользуемся тернарным оператором (if логика) и в зависимость от того авторизован пользователь или нет - будем отображать тот или иной div
+//P.S. При логике if мы можем отображать только один div за раз, но не сразу несколько. Поэтому заварачиваем все интересующие нас елементы под один div
+//Далее, чтобы елемент, в зависимости от действий юзера, перерендривался в режиме реального времени (чтобы mobx мог отслеживать изменения значений состояний и при изменении автоматически обновлять страницу) - обернем наш компонент в функцию observer()
+//И убедимся что все работает при помощи слушателя "onClick={() => user.setIsAuth(true)}" на кнопке аторизации. При нажатии - становимся авторизованими. setIsAuth(true) - это финкция которую мы создали в хранилище mobx
 import React, { useContext } from 'react'
 import {Context} from '../index'
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Button } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { SHOP_ROUTE } from '../utils/consts';
+import { observer } from 'mobx-react-lite';
 
-function NavBar() {
+const NavBar = observer(() => {
     const {user} = useContext(Context)
     return (
         <Navbar bg="light" expand="lg">
             <Container fluid>
-            <NavLink style={{color: 'blue'}} to={SHOP_ROUTE}>Max's Store</NavLink>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-                <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll
-                >
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown title="Link" id="navbarScrollingDropdown">
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                    Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                    Something else here
-                    </NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href="#" disabled>
-                    Link
-                </Nav.Link>
-                </Nav>
-                <Form className="d-flex">
-                <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-                </Form>
-            </Navbar.Collapse>
+                <NavLink style={{color: 'blue'}} to={SHOP_ROUTE}>Max's Store</NavLink>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    {user.isAuth ?
+                        <Nav className="ms-auto my-2 my-lg-0" style={{color: 'blue'}} navbarScroll> {/*P.S. ml-auto не работает, работает - ms-auto*/}
+                            <Button variant='outline-ligth'>Админ панель</Button>
+                            <Button variant='outline-ligth' className="ms-3">Выйти</Button>  {/*Чтобы кнопки не слипались добавим margin_start-3*/}                  
+                        </Nav>
+                    :
+                        <Nav className="ms-auto my-2 my-lg-0" style={{color: 'blue'}} navbarScroll> {/*P.S. ml-auto не работает, работает - ms-auto*/}                     
+                            <Button onClick={() => user.setIsAuth(true)} variant='outline-ligth'>Авторизация</Button>                                    
+                        </Nav>
+                    }                    
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
-}
+})
 
 export default NavBar
