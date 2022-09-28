2ec4c968ac1e72c086f293f6eb3bbfcd026bc4ad
mmitusov <maksym.mitusov@gmail.com>
2022-09-28 18:43:36 +0300

    Auth implementation

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -10,28 +10,49 @@
 //Чтобы расположить елементы в Row по разные стороны контейнера, то воспользуемся "d-flex justify-content-between". И теперь стили расположения кнопки Войти можно убрать
 //!!!P.S. Почему-то в новой версии bootstrap, при использовании Row у нас ничего не выравнивается. Но когда я заменил бутстраповский Row на обычный <div> - все заработало и выравнялось
 
+//Завершив форму логина, теперь мы должны дополнительно добавить форму регистрации (в этом же файле). Чтобы в зависимости от указанной в браузере ссылки (маршрута), у нас отображался либо логин, либо регистрация
+//Для этого воспользуемся хуком "useLocation" из 'react-router-dom'. С помощью него можно получить маршрут в строке запроса (маршрут хранится в ключе pathname)
+//И теперь когда маршрут из браузера будет совпадать с LOGIN_ROUT (true), мы будем отрисовывать часть кода с логином, а если нет, то с регистрацией
+//Чтобы не дублировать код, логику "или" можно было бы прописать только в тех частях где меняется текст/ссылки. Но для для лишей наглядности, я разделил полностью продублировал и разделил две логики
 import React from 'react' //rafce snippet
 import { Button, Card, Container, Form } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
-import { REGISTRATION_ROUTE } from '../utils/consts'
+import { NavLink, useLocation } from 'react-router-dom'
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts'
 
 const Auth = () => {
+  const location = useLocation()
+  const isLogin = location.pathname === LOGIN_ROUTE
+
   return (
     <Container 
       className='d-flex justify-content-center align-items-center'
       style={{height: window.innerHeight - 54}}
     >
-      <Card style={{width: 600}} className="p-5">
-        <h2 className='m-auto'>Авторизация</h2>
+      {isLogin ?
+        <Card style={{width: 600}} className="p-5">
+          <h2 className='m-auto'>Авторизация</h2>
+          <Form className='d-flex flex-column'>
+            <Form.Control placeholder="Введите ваш email" className='mt-2'/>
+            <Form.Control placeholder="Введите ваш пароль" className='mt-3'/>
+            <div className='d-flex justify-content-between mt-3'>
+              <div>Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегестрируйся!</NavLink></div>
+              <Button variant='outline-success'>Войти</Button>
+            </div>
+          </Form>
+        </Card>
+      :
+        <Card style={{width: 600}} className="p-5">
+        <h2 className='m-auto'>Регистрация</h2>
         <Form className='d-flex flex-column'>
           <Form.Control placeholder="Введите ваш email" className='mt-2'/>
           <Form.Control placeholder="Введите ваш пароль" className='mt-3'/>
           <div className='d-flex justify-content-between mt-3'>
-            <div>Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегестрируйся!</NavLink></div>
-            <Button variant='outline-success'>Войти</Button>
+            <div>Уже есть аккаунт? <NavLink to={LOGIN_ROUTE}>Заходи!</NavLink></div>
+            <Button variant='outline-success'>Зарегестрироваться</Button>
           </div>
         </Form>
       </Card>
+    }
     </Container>    
   )
 }
