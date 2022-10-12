0b614392bdf6aa49dcb464be5cae654967ba3028
mmitusov <maksym.mitusov@gmail.com>
2022-10-12 21:20:06 +0300

    DeviceList implementation

--- src/components/DeviceItem.js
+++ src/components/DeviceItem.js
@@ -1,11 +1,23 @@
-import React, { useContext } from 'react'
-import { Col } from 'react-bootstrap'
+//DeviceItem отвечает за внешний вид сетки с товарами и отображение той информации, что мы ранее получили с БД в DeviceList
+//При деструктуризации device={currDevice}, то мы деструктуризируем сам компонент device, но не его начинку - currDevice. Иначе ничего не работает
+//И также временно захардкодим название и его рейтинг
 
-const DeviceItem = ({currDevice}) => {    
+import React, { useContext } from 'react'
+import { Card, CardGroup, Image } from 'react-bootstrap'
+
+const DeviceItem = ({device}) => {    
     return (
-        <Col md='3'>
-            item
-        </Col>
+        <CardGroup md='3'>
+            <Card style={{width: 150, cursor: 'pointer'}} border={"light"}>
+                <Image width={150} height={150} src={device.img}/>
+                <div>
+                    <div> Samsung etc. </div>
+                    <div>
+                        <div> {device.raiting} </div>
+                    </div>
+                </div>
+            </Card>
+        </CardGroup>
     )
 }
 
--- src/components/DeviceList.js
+++ src/components/DeviceList.js
@@ -1,4 +1,7 @@
-//
+//Это входной компонент для отображениея сетки со списками товаров. Здесь мы первоначально пробегаемся по товарам из нашей БД
+//Далее, пропсами, мы передаем товары что мы получили из нашей БД, в следующий компонент - DeviceItem
+//И уже DeviceItem будет отвечать за внешний вид сетки с товарами и отображение той информации, что мы ранее получили с БД
+//Поэтому с получением каждого товара мы создаем отдельный DeviceItem и передаем в него пропс, по которому он будет отрисовывать конкретный товар
 
 import React, { useContext } from 'react'
 import {Context} from '../index'
@@ -12,7 +15,7 @@ const DeviceList = observer(() => {
     return (
         <Row class-className='d-flex'>
             {device.devices.map(currDevice =>
-                <DeviceItem key={device.id} device={currDevice} />
+                <DeviceItem key={currDevice.id} device={currDevice} />
             )}
         </Row>
     )
