c5cb9e231e03765b844999b08002013f57d4d9b7
mmitusov <maksym.mitusov@gmail.com>
2022-10-14 21:03:47 +0300

    DeviceItems.js implementation

--- src/components/DeviceItem.js
+++ src/components/DeviceItem.js
@@ -1,21 +1,27 @@
 //DeviceItem отвечает за внешний вид сетки с товарами и отображение той информации, что мы ранее получили с БД в DeviceList
 //При деструктуризации device={currDevice}, то мы деструктуризируем сам компонент device, но не его начинку - currDevice. Иначе ничего не работает
 //И также временно захардкодим название и его рейтинг
+//Создадим папку assets, где мы будем хранить разные медиа, и загрузим туда картинку звездочки для рейтинга товара
+//Выравниваем надписи, отлепим от картинки и сделаем их серыми: className='d-flex justify-content-between align-items-center mt-3 text-black-50'
+//Чтобы звездочка была на ровне (в одну линию) с оценкой: className='d-inline-flex'
 
 import React, { useContext } from 'react'
 import { Card, CardGroup, Image } from 'react-bootstrap'
+import starImg from '../assets/Star_full.png'
 
 const DeviceItem = ({device}) => {    
     return (
-        <CardGroup md='3'>
+        <CardGroup>
             <Card style={{width: 150, cursor: 'pointer'}} border={"light"}>
                 <Image width={150} height={150} src={device.img}/>
-                <div>
+                <div className='d-flex justify-content-between align-items-center mt-3 text-black-50'>
                     <div> Samsung etc. </div>
-                    <div>
+                    <div className='d-inline-flex align-items-center'>
                         <div> {device.raiting} </div>
-                    </div>
+                        <Image width={20} height={20} src={starImg}/>
+                    </div>                    
                 </div>
+                <div> {device.name} </div>
             </Card>
         </CardGroup>
     )
--- src/components/DeviceList.js
+++ src/components/DeviceList.js
@@ -1,7 +1,8 @@
 //Это входной компонент для отображениея сетки со списками товаров. Здесь мы первоначально пробегаемся по товарам из нашей БД
-//Далее, пропсами, мы передаем товары что мы получили из нашей БД, в следующий компонент - DeviceItem
+//Далее, пропсами, мы передаем массив информации о товарах, что мы получили из нашей БД, в следующий компонент - DeviceItem
 //И уже DeviceItem будет отвечать за внешний вид сетки с товарами и отображение той информации, что мы ранее получили с БД
 //Поэтому с получением каждого товара мы создаем отдельный DeviceItem и передаем в него пропс, по которому он будет отрисовывать конкретный товар
+//Также параметры сетки в bootstrap завдаются в <Row>. Поэтому указываем в нем нужные пераметры сетки для DeviceItem: xs={2} md={3}
 
 import React, { useContext } from 'react'
 import {Context} from '../index'
@@ -13,7 +14,7 @@ const DeviceList = observer(() => {
     const {device} = useContext(Context)
     
     return (
-        <Row class-className='d-flex'>
+        <Row class-className='d-flex' xs={2} md={4}>
             {device.devices.map(currDevice =>
                 <DeviceItem key={currDevice.id} device={currDevice} />
             )}
