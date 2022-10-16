312f4f0034ee13fd23ece6dc6e9d74867cf12cc8
mmitusov <maksym.mitusov@gmail.com>
2022-10-16 20:37:42 +0300

    DevicePage.js implementation

--- src/components/DeviceItem.js
+++ src/components/DeviceItem.js
@@ -4,15 +4,25 @@
 //И также временно захардкодим название и его рейтинг
 //Создадим папку assets, где мы будем хранить разные медиа, и загрузим туда картинку звездочки для рейтинга товара
 //Выравниваем надписи, отлепим от картинки и сделаем их серыми: className='d-flex justify-content-between align-items-center mt-3 text-black-50'
-//Чтобы звездочка была на ровне (в одну линию) с оценкой: className='d-inline-flex' 
+//Чтобы звездочка была на ровне (в одну линию) с оценкой: className='d-inline-flex'
 
-import React, { useContext } from 'react'
+//Далее сделаем товары кликабельными, чтобы переходить на страницу конкретного товара при клике на него
+//Для этого импортируем хук useNavigate из 'react-router-dom', который поможет нам динамически передвигаться по страницам
+//Вешаем слушатель события onClick на конкретный елемент-карточку и вызываем функцию navigate (useNavigate()). В старой версии нужно было вызывать navigate.push()
+//В функцию передаем DEVICE_ROUTE вместе с id-шником конкретного товара '+ '/' + device.id', чтобы при нажатии на товар перемещатся прямиком на адрес его страницы
+//Тоесть мы выцепляем id-шник конкретного товара из БД и по нему делаем запрос на сервер, для получения данных о товаре (нас полностью преводит с текущей страницы на страницу с товаром)
+//
+
 import { Card, CardGroup, Image } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 import starImg from '../assets/Star_full.png'
+import { DEVICE_ROUTE } from '../utils/consts'
 
-const DeviceItem = ({device}) => {    
+const DeviceItem = ({device}) => {
+    const navigate = useNavigate()
+    
     return (
-        <CardGroup>
+        <CardGroup onClick={() => navigate(DEVICE_ROUTE + '/' + device.id)}>
             <Card style={{width: 150, cursor: 'pointer'}} border={"light"}>
                 <Image width={150} height={150} src={device.img}/>
                 <div className='d-flex justify-content-between align-items-center mt-3 text-black-50'>
--- src/pages/DevicePage.js
+++ src/pages/DevicePage.js
@@ -1,9 +1,26 @@
+//Это страница которая будет отображать детали о конкретном товаре, при выборее его из перечня всех товаров в магазине
+//Опять же, весь компонент оборачиваем в контейнер. Добавим в контейнер 3 колонки: изображение товара, рейтинг и функцию "добавить в корзину". 
+//И разобъем колонки по 4 столбца, чтобы все было поровну: md={4}
+//Начнем с колонки для изображения товара
+
 import React from 'react'
-import {Switch, Route, Redirect} from 'react-router-dom'
+import { Col, Container, Image } from 'react-bootstrap'
 
 const DevicePage = () => {
+const device = {id: 1, name: "iPhone 14", price: 800, raiting: 5, img: "https://www.apple.com/newsroom/images/product/iphone/geo/Apple_iphone13_hero_geo_09142021_inline.jpg.large.jpg"}
+
   return (
-    <div>DevicePage</div>
+    <Container>
+      <Col md={4}>
+        <Image width={300} height={300} src={device.img}/>
+      </Col>
+      <Col md={4}>
+        
+      </Col>
+      <Col md={4}>
+        
+      </Col>      
+    </Container>
   )
 }
 
