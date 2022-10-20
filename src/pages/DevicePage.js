f8919f33ce309d533dd90e78b8966d6e9ace012a
mmitusov <maksym.mitusov@gmail.com>
2022-10-20 18:00:05 +0300

    DevicePage.js implementation

--- src/pages/DevicePage.js
+++ src/pages/DevicePage.js
@@ -13,7 +13,12 @@
 //После чего обернем все <Col> в один общий <Row>, чтобы все наши колонки отобразились в один горизонтальный ряд, а не шли одна под другой
 //Мы используем колонки, чтобы ограничить контейнер по ширине. Иначе наши контейнера занимал бы всю ширину и тогда мы не смогли бы расположить их в ряд (так как у каждого из них уже занята вся ширина)
 
-//Далее займемся колонкой по добавлению товара в корзину     
+//Далее займемся колонкой по добавлению товара в корзину
+//Зададим ей параметры: className='d-flex flex-column align-items-center justify-content-around'; style={{width:300, height:300, fontSize:32, border: '5px solid lightgrey'}}
+//Теперь оталось добавить характеристики товара. Под это создадим отдельный массив: ПОТОМ БУДЕМ ПОЛУЧАТЬ С БЕКА
+//В самый низ добавляем еще один Row под характеристики
+//Пробегаемся по БД и вытягиваем характеристики. Каждую из них отрисовываем в собственную строку (название: описание).
+//Чтобы сделать строки разных цветов, то каждую характеристику под четным индексом будем отрисовывать с серым фоном
 //P.S. Если используем styles то задаем ширину как: style={widht:300}; А если указываем ширину в div, то: <Image width={300}/>
 
 import React from 'react'
@@ -22,13 +27,17 @@ import bigStarImg from '../assets/Star_outline.png'
 
 const DevicePage = () => {
 const device = {id: 1, name: "iPhone 14", price: 800, raiting: 5, img: "https://www.apple.com/newsroom/images/product/iphone/geo/Apple_iphone13_hero_geo_09142021_inline.jpg.large.jpg"}
+const description = [
+  {id: 1, title:"RAM", description:'8 гб'},
+  {id: 2, title:"ROM", description:'64 гб'},
+  {id: 3, title:"BOOM", description:'128 гб'}
+]
 
   return (
     <Container className='mt-3'>
-      <Row>
-        
-        <Col md={4}>
-          <Image width={300} height={300} src={device.img}/>
+      <Row>        
+        <Col md={4} className='d-flex align-items-center justify-content-center'>
+          <Image width={250} height={300} src={device.img}/>
         </Col>
 
         <Col md={4}> {/* Но если здесь мы завернем <Col> в <Row>, то стили не будут работать (в новой версии бутстрап так делать не желательно)*/}
@@ -44,13 +53,24 @@ const device = {id: 1, name: "iPhone 14", price: 800, raiting: 5, img: "https://
         </Col>
 
         <Col md={4}>
-          <Card>
-            <h3> {device.price} </h3>
+          <Card
+            className='d-flex flex-column align-items-center justify-content-around'
+            style={{width:300, height:300, fontSize:32, border: '5px solid lightgrey'}}
+          >
+            <h3> От {device.price} долл.</h3>
             <Button variant='outline-dark'>Добавить в корзину</Button>
           </Card>
         </Col>
+      </Row>
 
-      </Row>            
+      <Row className='d-flex flex-column m-3'>
+        <h1>Характеристики</h1>
+        {description.map((info, index) => 
+          <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgrey' : 'transparent', padding:10}}> 
+            {info.title}: {info.description}
+          </Row>   
+        )}
+      </Row>          
     </Container>
   )
 }
