d5c42a16666956c2754a4e6b8f475767aa0dea72
mmitusov <maksym.mitusov@gmail.com>
2022-10-21 20:14:13 +0300

    Admin.js implementation

---
+++ src/components/modals/CreateBrand.js
@@ -0,0 +1,41 @@
+//Этот компонент используется в Admin.js, для добавления новых типов товаров на БД. Компонент идентичен с CreateType.js
+//Копируем из react-bootstrap (раздел Modal) компонент под названием Vertically centered и импортируем его зависимости import { Button, Modal }
+//Удаляем все стили/настройки из тега <Modal> и все вложеные теги из <Modal.Body>
+//Далее передаем в компонент 2 пропса: show - отвечает виден компонент или нет (принимает true/false); onHide - функция которая скрывает модальное окно
+//show и onHide - это бутстраповские елементы, мы их не создаем где-то отдельно, а просто используем из коробки
+//И опрокидываем оба пропса в корень модального окна, в тег <Modal>
+//Внутрь тела модального окна добавим форму <Form>, а в саму форму добавляем уже инпут <Form.Control />
+//И добавим еще одну кнопку. Одна - для добавления нового типа товара, другая - для закрытия модального окна
+
+import React from 'react'
+import { Button, Form, Modal } from 'react-bootstrap'
+
+const CreateBrand = ({show, onHide}) => {
+  return (
+    <Modal
+    show={show}
+    onHide={onHide}   
+    size="lg"    
+    centered
+  >
+    <Modal.Header closeButton>
+      <Modal.Title id="contained-modal-title-vcenter">
+        Добавить новый бренд товара
+      </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Form>
+        <Form.Control 
+          placeholder={'Введите название бренда'}
+        />
+      </Form>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant='outline-success' onClick={onHide}>Добавить бренд</Button>
+      <Button variant='outline-danger' onClick={onHide}>Закрыть окно</Button>
+    </Modal.Footer>
+  </Modal>
+  )
+}
+
+export default CreateBrand
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,16 +1,37 @@
 //Наша админ панель
 //Сперва завернем компонеот в Container
-//Container Component provides a way to center and horizontally pad the contents of our application. It is used when the user wants the responsive pixel width. 
+//Container Component provides a way to center and horizontally pad the contents of our application. It is used when the user wants the responsive pixel width.
+//Добавим три кнопки. Настроим их стиль и отлепим их друг от друга ('mt-2')
+//При нажатии на каждую из кнопок должно всплывать модальное окно, с разными инпутами. Для этого повесим на кнопки слушатель событий
+//А функционал модального окна возмем из react-bootstrap и в разделе Modal, выберем Vertically centered
+//Для чистоты кода, мы не будем добавлять код модальных окон прямиком в Admin.js, а вынесем их как отдельные компоненты. И будем уже импортировать их от туда
+//Для этого создадим под данные елементы новую папку modals внутри папки components. Где каждое модальное окно будет как отдельный компонент 
+//Импортируем наши новые компонеты отдельно от кнопок (под ними). Ведь окно будет появляться не в пределах одной кнопки, а на весь екран
+//Каждая кнопка буде включать появление каждого отдельного модального окна
+//Далее создадим три состояния (с помощью хуков), которые будут отвечать за то, видим ли мы модальное окно или нет
 
 import React from 'react'
 import { Button, Container } from 'react-bootstrap'
+import CreateBrand from '../components/modals/CreateBrand'
+import CreateDevice from '../components/modals/CreateDevice'
+import CreateType from '../components/modals/CreateType'
 
 const Admin = () => {
   return (
-    <Container>
-      <Button>Добавить тип</Button>
-      <Button>Добавить бренд</Button>
-      <Button>Добавить устройство</Button>
+    <Container className='d-flex flex-column'>
+      <Button variant={'outline-dark'} className='mt-4 p-2'>
+        Добавить тип
+      </Button>
+      <Button variant={'outline-dark'} className='mt-4 p-2'>
+        Добавить бренд
+      </Button>
+      <Button variant={'outline-dark'} className='mt-4 p-2'>
+        Добавить устройство
+      </Button>
+
+      <CreateBrand />
+      <CreateDevice />
+      <CreateType />
     </Container>
   )
 }
