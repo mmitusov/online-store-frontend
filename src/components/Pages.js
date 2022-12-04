907d5fcb2be81dbfbf4d34d6d72d5d20b6894a37
mmitusov <maksym.mitusov@gmail.com>
2022-12-04 21:50:17 +0200

    Pagination implementation

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -8,7 +8,16 @@
 //И теперь когда у нас есть первоначальная визуальная часть, экспортируем этот компонет в Shop.js и отобразим его в самом низу страницы
 //Далее перейдем в deviceStore.js и создадим там новые поля/состояния которые нам будут нужны для работы логики по пагинации: this._page, this._totalCount, this._limit
 //После чего перейдем в deviceAPI.js, чтобы дописать логику по фетчингу с бекенда нужной нам информации для работы пагинации и дальнейшем ее сохранением в deviceStore.js
+//А затем в Shop.js напишем логику по сохранению в mobx значения общего количества товаров, что нам пришли с бека
+//И теперь зная сколько всего товаров к нам пришло, мы можем посчитать сколько страниц для пагинации нам нужно отрисовать
+//Делается это просто: делим общее кол-во товаров на кол-во товаров, что отображаются на одной странице
+//И будем хранить результат подсчета в pageCount
+//Временное решение "[1,2,3,4,5]" - можно теперь удалить, и в место этого, при помощи цикла, мы теперь будем заполнять массив pages автоматически
 
+//После чего, теперь нам нужно как то визуально выделить текущею страницу (сделать чтобы она посвечивалась). Для этого в Pagination.Item, добавляем пропс "active"
+//"active" - это зашитая бутстраповская логика. Сами мы этот пропс не создаем
+//"active" будет равен true или false, в зависимости от того, если страница из памяти mobx равняется текущей странице
+//Далее добавляем слушатель события нажатия onClick={() => device.setPage(page). И onClick будет срабатывать только для той итерации page, на которую мы нажимаем
 
 import { observer } from 'mobx-react-lite';
 import React, { useContext } from 'react'
@@ -17,12 +26,20 @@ import Pagination from 'react-bootstrap/Pagination';
 
 const Pages = observer( () => {
   const {device} = useContext(Context)
-  const pages = [1,2,3,4,5]
+  const pageCount = Math.ceil(device.totalCount / device.limit)
+  const pages = []
+  for (let i=0; i<pageCount; i++) {
+    pages.push(i+1)
+  }
 
   return (
     <Pagination className='mt-5'>
       {pages.map(page => 
-        <Pagination.Item>
+        <Pagination.Item 
+          key={page}
+          active={device.page === page}    
+          onClick={() => device.setPage(page)}      
+        >
           {page}
         </Pagination.Item> 
       )}
